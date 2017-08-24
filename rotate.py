fc42fdca20d360cab9595919579b07e8c9c302ce
Jon Leete <jleete97@gmail.com>
2017-08-24 15:18:34 -0500

    fixed rotation of shape

---
+++ rotate.py
@@ -0,0 +1,16 @@
+import math
+
+rotationMatrices = {}
+
+for deg in range(360):
+    rad = deg * math.pi / 180.0
+    rotationMatrix = [
+        [math.cos(rad), -math.sin(rad)],
+        [math.sin(rad), math.cos(rad)]
+    ]
+    rotationMatrices[deg] = rotationMatrix
+
+def rotate(p, deg):
+    rm = rotationMatrices[deg]
+    return [p[0] * rm[0][0] + p[1] * rm[0][1],
+            p[0] * rm[1][0] + p[1] * rm[1][1]]
