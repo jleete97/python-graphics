fde9cdb49916066e3b0950ae24142355fd6d5df0
Jon Leete <jleete97@gmail.com>
2017-09-03 11:42:13 -0400

    second gravity simulation working, with suns and planet

--- gravity2.py
+++ gravity2.py
@@ -8,12 +8,15 @@ from movers import *
 # Window parameters
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 900
+window = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
 # Sun parameters
 SUN_RADIUS = 30
 SUN_POS = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
 GRAVITY_CONSTANT = 30.0
 TOO_CLOSE = SUN_RADIUS * 2
 
+BACKGROUND_COLOR = (128, 128, 128)
 
 
 # Initialize
@@ -22,7 +25,23 @@ mainClock = pygame.time.Clock()
 surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
 pygame.display.set_caption("Gravity")
 
-ship = SpaceShip((WINDOW_WIDTH // 20, WINDOW_HEIGHT // 20))
+# Define things moving around the screen (or not)
+ship = SpaceShip((WINDOW_WIDTH // 20, WINDOW_HEIGHT // 20), limits = window)
+sun1 = Sun((WINDOW_WIDTH // 3, WINDOW_HEIGHT // 2), mass = 1000, diameter = 20)
+sun2 = Sun((2 * WINDOW_WIDTH // 3, WINDOW_HEIGHT // 2), mass = 300, diameter = 10)
+planet1 = Planet((sun2.pos[0] + WINDOW_WIDTH // 30, sun2.pos[1]), 70, (0, 2))
+
+sun1.motion = (0, 0.55)
+sun2.motion = (0, -2)
+planet1.color = GREEN
+
+movers = [sun1, sun2, planet1, ship]
+#movers = [sun1, ship]
+
+def isQuitEvent(event):
+    return event.type == QUIT \
+        or ( (event.type == KEYUP) \
+              and (event.key == K_ESCAPE or event.key == ord('q')) )
 
 # Main event loop
 while True:
@@ -30,53 +49,28 @@ while True:
     # Handle events: rotation, thrust, color change, quit
 
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if isQuitEvent(event):
             pygame.quit()
             sys.exit()
-        elif event.type == KEYDOWN:
-            if event.key == K_UP:
-                thrust = 1
-            elif event.key == K_LEFT:
-                rotation = -10
-            elif event.key == K_RIGHT:
-                rotation = 10
-        elif event.type == KEYUP:
-            if event.key == K_ESCAPE or event.key == ord('q'):
-                pygame.quit()
-                sys.exit()
-            elif event.key == K_LEFT or event.key == K_RIGHT:
-                rotation = 0
-            elif event.key == K_UP:
-                thrust = 0
-            elif event.key == ord('c'):
-                color = (color + 1) % len(PLAYER_COLORS)
+        else:
+            handled = ship.handle(event)
 
     # Calculate motion and position changes
 
-    # Add rotation to direction the ship is facing
-    direction = (direction + rotation) % 360
+    for mover in movers:
+        mover.calculateMotionChange(movers)
 
-    # Add result of thrust to motion
-    motion = offsetRotate(motion, (0, thrust), direction)
-
-    # Add effect of gravity
-    gravityScale = GRAVITY_CONSTANT / ((pos[0] - SUN_POS[0]) ** 2 + (pos[1] - SUN_POS[1]) ** 2)
-    gravityForce = ((SUN_POS[0] - pos[0]) * gravityScale, (SUN_POS[1] - pos[1]) * gravityScale)
-    motion = (motion[0] + gravityForce[0], motion[1] + gravityForce[1])
-
-    # Increment position; wrap around as needed
-    pos = ((pos[0] + motion[0]) % WINDOW_WIDTH, (pos[1] + motion[1]) % WINDOW_HEIGHT)
-
-    # Determine distance from sun for later comparison to TOO_CLOSE, to set exploded flag
-    distanceFromSun = math.sqrt((pos[0]-SUN_POS[0]) ** 2 + (pos[1]-SUN_POS[1]) ** 2)
+    for mover in movers:
+        mover.incrementPosition()
 
     # Draw
 
     # Background
-    surface.fill(DARKGREY)
-    # Sun
-    pygame.draw.circle(surface, YELLOW, (SUN_POS), SUN_RADIUS)
-    # Player's ship
+    surface.fill(BACKGROUND_COLOR)
+
+    # Various bodies
+    for mover in movers:
+        mover.draw(surface)
 
     pygame.display.update()
     mainClock.tick(80)
--- movers.py
+++ movers.py
@@ -1,33 +1,82 @@
 import pygame, math
 from rotate import rotate, offsetRotate
+import pygame
+from pygame.locals import *
 
 GRAVITY_CONSTANT = 2.0
 
 class DrawableMover(object):
 
-    motion = (0, 0)
-
-    def __init__(self, pos, mass, seesGravity = True):
+    def __init__(self, pos, mass, seesGravity = True, limits = None, limitStrategy = 'wrap'):
         self.pos = pos
+        self.mass = mass
         self.seesGravity = seesGravity
+        self.motion = (0, 0)
+        self.accel = (0, 0)
+        self.limits = limits
+        self.limitStrategy = limitStrategy
 
-    def moveAndAdjustMotion(self, masses):
-        self.pos = (self.pos[0] + self.motion[0], self.pos[1] + self.motion[1])
+    def calculateMotionChange(self, otherMasses):
+        if self.seesGravity:
+            self.accel = self.accelerationFromOtherMasss(otherMasses)
 
-        for mass in masses:
-            if mass.mass != 0:
-                delta = (mass.pos[0] - self.pos[0], mass.pos[1] - self.pos[1])
+    def accelerationFromOtherMasss(self, otherMasses):
+        totalAcceleration = (0, 0)
+
+        for otherMass in otherMasses:
+            if otherMass != self and otherMass.mass != 0:
+                delta = (otherMass.pos[0] - self.pos[0], otherMass.pos[1] - self.pos[1])
                 distSquared = delta[0] ** 2 + delta[1] ** 2
-                dist = math.sqrt(distSquared)
-                rawAccel = GRAVITY_CONSTANT * mass.mass / distSquared
-                accel = (rawAccel * (delta[0] / dist), rawAccel * (delta[1] / dist))
-                self.motion = (self.motion[0] + accel[0], self.motion[1] + accel[1])
+
+                if (distSquared > 0):
+                    dist = math.sqrt(distSquared)
+                    rawAccel = GRAVITY_CONSTANT * otherMass.mass / distSquared
+                    accel = (rawAccel * (delta[0] / dist), rawAccel * (delta[1] / dist))
+                else:
+                    accel = (0, 0)
+
+                totalAcceleration = (totalAcceleration[0] + accel[0],
+                                     totalAcceleration[1] + accel[1])
+
+        return totalAcceleration
+
+    def incrementPosition(self):
+        self.pos = (self.pos[0] + self.motion[0],
+                    self.pos[1] + self.motion[1])
+
+        if self.limits is not None:
+            if (self.limitStrategy == 'wrap'):
+                self.pos = wrap(self.pos, self.limits)
+            else:
+                self.motion = bouncedMotion(self.motion, self.pos, self.limits)
+                self.pos = bouncedPosition(self.pos, self.limits)
+
+        self.motion = (self.motion[0] + self.accel[0],
+                       self.motion[1] + self.accel[1])
 
     def draw(self, surface):
         raise BaseException("I don't know how to draw myself")
 
+def wrap(pos, limits):
+    return (pos[0] % limits[0], pos[1] % limits[1])
+
+def bouncedMotion(motion, pos, limits):
+    return (motion[0] if pos[0] > 0 and pos[0] < limits[0] else -motion[0],
+            motion[1] if pos[1] > 0 and pos[1] < limits[1] else -motion[1])
+
+def bouncedPosition(pos, limits):
+    return (bounce(pos[0], limits[0]), bounce(pos[1], limits[1]))
+
+def bounce(coord, limit):
+    if coord < 0:
+        return -coord
+    elif coord >= limit:
+        return coord - (limit - coord + 1)
+    else:
+        return coord
+
 # Ship size parameters
-PLAYER_WIDTH = 10
+PLAYER_WIDTH = 6
 PLAYER_LENGTH = PLAYER_WIDTH + PLAYER_WIDTH // 2
 
 # Explosion size parameters
@@ -45,6 +94,7 @@ PURPLE = (255, 0, 255)
 TURQUOISE = (0, 255, 255)
 WHITE = (255, 255, 255)
 GREY = (192, 192, 192)
+BROWN = (128, 128, 0)
 
 # Player can be any color except color of background, flame, sun,
 # or black (until they explode, when they stay black)
@@ -54,12 +104,24 @@ ROTATION_INCREMENT = 10
 
 class SpaceShip(DrawableMover):
 
-    exploded = False
-    direction = 0
-    color = 0
-    rotation = 0
-    explosionDiameter = 10
-    thrust = 0
+    def __init__(self, pos, seesGravity = True, limits = None, limitStrategy = 'wrap'):
+        super().__init__(pos, 0, True, limits = limits, limitStrategy = limitStrategy)
+
+        self.direction = 0
+        self.rotation = 0
+        self.thrust = 0
+        self.colorIndex = 0
+        self.exploded = False
+        self.explosionDiameter = 10
+
+    def calculateMotionChange(self, otherMasses):
+        super().calculateMotionChange(otherMasses)
+
+        # Add rotation to direction the ship is facing
+        self.direction = (self.direction + self.rotation) % 360
+
+        # Add result of thrust to motion
+        self.motion = offsetRotate(self.motion, (0, self.thrust), self.direction)
 
     def rotate(self, direction):
         if direction == 0:
@@ -69,20 +131,47 @@ class SpaceShip(DrawableMover):
         else:
             self.rotation = ROTATION_INCREMENT
 
+
+    def handle(self, event):
+        if (event.type == KEYUP):
+            if event.key == K_UP:
+                self.thrust = 0
+            elif event.key == K_LEFT or event.key == K_RIGHT:
+                self.rotation = 0
+            else:
+                return False
+        elif (event.type == KEYDOWN):
+            if event.key == K_UP:
+                self.thrust = 1
+            elif event.key == K_LEFT:
+                self.rotation = -10
+            elif event.key == K_RIGHT:
+                self.rotation = 10
+            elif event.key == ord('c'):
+                self.colorIndex = (self.colorIndex + 1) % len(PLAYER_COLORS)
+            else:
+                return False # no case matched, we didn't handle the event
+        else:
+            return False # not our type of event; we didn't handle.
+
+        return True # we handled the event.
+
     def draw(self, surface):
+        drawPos = (int(self.pos[0]), int(self.pos[1]))
         offsetl = PLAYER_WIDTH * 2
         offsetw = PLAYER_WIDTH
-        front = offsetRotate(self.pos, (0, offsetl), self.direction)
+        front = offsetRotate(drawPos, (0, offsetl), self.direction)
         leftShoulder = offsetRotate(self.pos, (offsetw, -offsetl), self.direction)
         rightShoulder = offsetRotate(self.pos, (-offsetw, -offsetl), self.direction)
-        shipColor = BLACK if self.exploded else PLAYER_COLORS[self.color]
+        shipColor = BLACK if self.exploded else PLAYER_COLORS[self.colorIndex]
         pygame.draw.polygon(surface,
                             shipColor,
                             (front, rightShoulder, leftShoulder))
+
         if (self.thrust > 0):
-            flametip = offsetRotate(self.pos, (0, - offsetl * 2), self.direction)
-            flamels = offsetRotate(self.pos, (offsetw // 2, -offsetl), self.direction)
-            flamers = offsetRotate(self.pos, (-offsetw // 2, -offsetl), self.direction)
+            flametip = offsetRotate(drawPos, (0, - offsetl * 2), self.direction)
+            flamels = offsetRotate(drawPos, (offsetw // 2, -offsetl), self.direction)
+            flamers = offsetRotate(drawPos, (-offsetw // 2, -offsetl), self.direction)
             pygame.draw.polygon(surface,
                                 RED,
                                 (flametip, flamers, flamels))
@@ -97,10 +186,25 @@ class SpaceShip(DrawableMover):
 #            pygame.draw.circle(surface, explosionColor, ((int(pos[0]), int(pos[1]))), explosionDiameter)
 #            explosionDiameter += 1
 
-SOLAR_MASS = 50
+class Sun(DrawableMover):
+    def __init__(self, pos, mass, diameter = 50, color = YELLOW, seesGravity = True):
+        super().__init__(pos, mass, seesGravity)
+        self.diameter = diameter
+        self.color = color
 
-class Sun:
-    mass = SOLAR_MASS
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (intPos(self.pos)), self.diameter)
 
-    def __init__(self, pos):
-        self.pos = pos
+
+class Planet(DrawableMover):
+    def __init__(self, pos, mass, initialMotion, diameter = 5, color = BROWN, seesGravity = True):
+        super().__init__(pos, mass, seesGravity)
+        self.diameter = diameter
+        self.color = color
+        self.motion = initialMotion
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (intPos(self.pos)), self.diameter)
+
+def intPos(pos):
+    return (int(pos[0]), int(pos[1]))
